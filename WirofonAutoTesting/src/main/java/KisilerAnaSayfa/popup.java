c54ed3023fd2ddc355342aa10481438fc83c8316
Firat Aras <firat.aras@argela.com.tr>
2016-05-26 09:23:36 +0300

    popup

--- WirofonAutoTesting/src/main/java/KisilerAnaSayfa/popup.java
+++ WirofonAutoTesting/src/main/java/KisilerAnaSayfa/popup.java
@@ -8,18 +8,24 @@ import io.appium.java_client.android.AndroidDriver;
 
 public class popup {
 	public static void clickSesliAra(AndroidDriver<MobileElement> driver){
+		
 	
 	}
 	public static void clickGoruntuluAra(AndroidDriver<MobileElement> driver){
+		SpesificMobilElement.clickByText(driver, Enumarations.GoruntuluAra);
 		
 	}
 	public static void clickMobilAra(AndroidDriver<MobileElement> driver){
+		SpesificMobilElement.clickByText(driver, Enumarations.MobilAra);
+
 	}
 	public static void clickSil(AndroidDriver<MobileElement> driver){
 		SpesificMobilElement.clickByText(driver, Enumarations.Sil);
 	
 	}
 	public static void clickSohbet(AndroidDriver<MobileElement> driver){
+		SpesificMobilElement.clickByText(driver, Enumarations.Sohbet);
+
 	}
 	public static void clickWirofonPopUpTamam(AndroidDriver<MobileElement> driver){
 		SpesificMobilElement.clickByText(driver, Enumarations.WirofonPopupTamam);
